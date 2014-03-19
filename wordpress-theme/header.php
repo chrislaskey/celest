7e8bda7fa224442c30eccdc52ca0ff6b2b9ff44a
Chris Laskey <contact@chrislaskey.com>
2014-03-18 20:42:41 -0400

    Blocking out header file

---
+++ wordpress-theme/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+
+    include('functions-head.php');
+    
+?><!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body <?php body_class(' page-'.implode(' page-', $uri)); ?>>
