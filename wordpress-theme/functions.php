ba2cf6243bde15a18775c5ebcb82da9fc61bc29f
Chris Laskey <contact@chrislaskey.com>
2014-03-18 20:20:32 -0400

    Added default theme functions

--- wordpress-theme/functions.php
+++ wordpress-theme/functions.php
@@ -166,6 +166,67 @@
     }
 
 
+//WordPress functions from default theme
+
+    if( !function_exists('cat_list') ){
+
+        function cat_list() {
+            return term_list('category', ', ', 'Categories: %s', 'Also posted in %s');
+        }
+
+    }
+
+    if( !function_exists('tag_list') ){
+
+        function tag_list() {
+            return term_list( 'post_tag', ', ', 'Tags: %s', 'Also tagged %s');
+        }
+
+    }
+
+    if( !function_exists('term_list') ){
+
+        /**
+         * Updated v.1.0.1
+         * CL
+         */
+
+        function term_list($taxonomy, $glue = ', ', $text = '', $also_text = '') {
+
+            global $post, $wp_query;
+            $current_term = $wp_query->get_queried_object();
+            $terms = wp_get_object_terms($post->ID, $taxonomy);
+
+            // If we're viewing a Taxonomy page..
+            if ( isset( $current_term->taxonomy ) && $taxonomy == $current_term->taxonomy ) {
+
+                // Remove the term from display.
+                foreach ( $terms as $key => $term ) {
+                    if ( $term->term_id == $current_term->term_id ) {
+                        unset( $terms[$key] );
+                        break;
+                    }
+                }
+
+                // Change to Also text as we've now removed something from the terms list.
+                $text = $also_text;
+
+            }
+
+            $tlist = array();
+            $rel = 'category' == $taxonomy ? 'rel="category"' : 'rel="tag"';
+            foreach ( (array) $terms as $term ) {
+                $tlist[] = '<a href="' . get_term_link( $term, $taxonomy ) . '" title="' . esc_attr( sprintf('View all posts in %s', $term->name ) ) . '" ' . $rel . '>' . $term->name . '</a>';
+            }
+
+            if ( ! empty( $tlist ) )
+                return sprintf( $text, join( $glue, $tlist ) );
+            return '';
+        }
+
+    }
+
+
 //Functions for interacting with CI Access library
 
     function user_verify($verb, $type, $arguments){
