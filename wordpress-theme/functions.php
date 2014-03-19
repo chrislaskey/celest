498dac6cdf5fb7e4ea4860b208658063c619bafb
Chris Laskey <contact@chrislaskey.com>
2014-03-18 20:13:30 -0400

    Added common functions file

---
+++ wordpress-theme/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+
+//Definitions
+	if( ! defined('CL_TEMPLATEPATH') ){ define('CL_TEMPLATEPATH', '/wp-content/themes/celest-responsive'); }
