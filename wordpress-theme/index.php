ffa66ce2f86e8ddd137c9157911ccdc88a932950
Chris Laskey <contact@chrislaskey.com>
2014-03-19 12:04:59 -0400

    Removed extra html tags from index template

---
+++ wordpress-theme/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+
+?><?php include(TEMPLATEPATH.'/header.php'); ?>
+
+        <section id="content">
+
+            <?php get_template_part( 'loop', 'index' ); ?>
+
+        </section>
+
+<?php include(TEMPLATEPATH.'/footer.php'); ?>
