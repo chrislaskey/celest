3aea27453889cc1b992f548872b6ac9d5d37b2de
Chris Laskey <contact@chrislaskey.com>
2014-03-26 11:15:30 -0400

    Added honors overview template

---
+++ wordpress-theme/template-honors-overview.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template Name: Honors Overview
+ *
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+?><?php include(TEMPLATEPATH.'/header.php'); ?>
+
+        <?php the_post(); ?>
+
+        <section id="content" <?php post_class('page'); ?>>
+
+            <h1 class="title page_title"><?php the_title(); ?></h1>
+
+            <div class="post_content">
+
+                <?php the_content(); ?>
+
+                <?php
+
+                    $data = get_api_data('/api/our-work/honors-and-awards');
+
+                    if( isset($data->honors) && !empty($data->honors) ){
+
+                        $honors = array();
+
+                        foreach($data->honors as $honor){
+                            $honors[] = '<li>'.$honor->description.'</li>';
+                        }
+
+                        echo '<ol class="our-work-list honors">'.implode($honors).'</ol>';
+
+                    }else{
+
+                        echo '<p>Currently there are no CELEST Honors or Awards to display for the specified time period.</p>';
+
+                    }
+
+                ?>
+
+            </div>
+
+        </section>
+
+<?php include(TEMPLATEPATH.'/footer.php'); ?>
