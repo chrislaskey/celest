5498a993f17881946d892cc05cbef8ba520f2a1a
Chris Laskey <contact@chrislaskey.com>
2014-03-26 10:19:07 -0400

    Added publications overview template

---
+++ wordpress-theme/template-publications-overview.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template Name: Publications Overview
+ *
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+?><?php include(TEMPLATEPATH.'/header.php'); ?>
+
+        <?php the_post(); ?>
+
+        <section id="content" <?php post_class('page'); ?>>
+
+            <h1 class="title page_title"><?php the_title(); ?></h1>
+
+            <div class="post_content">
+
+                <?php the_content(); ?>
+
+                <?php
+
+                    $data = get_api_data('/api/our-work/publications');
+
+                    if( count($data->latest_book) > 0 ){
+
+                        echo '<h2>Most Recent Books and Book Chapters</h2>';
+                        echo '<ol class="our-work-list publications book-publications">';
+
+                        foreach($data->latest_book as $pub){
+                            echo '<li>'.create_book_publication($pub).'</li>';
+                        }
+                        
+                        echo '</ol>';
+
+                    }
+
+                    if( count($data->latest_peer) > 0 ){
+
+                        $nonconf_array = array();
+
+                        echo '<h2>Most Recent Peer-Reviewed Publications</h2>';
+                        echo '<ol class="our-work-list publications peer-publications">';
+
+                        foreach($data->latest_peer as $pub){
+                            echo '<li>'.create_peer_publication($pub).'</li>';
+                        }
+
+                        echo '</ol>';
+
+                    }
+
+                ?>
+
+            </div>
+
+        </section>
+
+<?php include(TEMPLATEPATH.'/footer.php'); ?>
