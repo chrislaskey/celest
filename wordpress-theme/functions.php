cc0ddaa39a642e814c1347bd3cbbcb9ea41c9f16
Chris Laskey <contact@chrislaskey.com>
2014-03-26 20:02:28 -0400

    Moved news article functions to functions file

--- wordpress-theme/functions.php
+++ wordpress-theme/functions.php
@@ -166,6 +166,94 @@
     }
 
 
+//News article footer functions
+
+    function display_news_articles($total = 3){
+        $i = 0;
+        $per_row = 3;
+        $posts = return_news_posts();
+        if( empty($posts) ){ return; }
+
+        foreach( $posts as $post ){
+            $i++;
+            $content = process_news_article($post);
+            $class = return_news_articles_class($i, $per_row);
+            print_bucket($content, $class);
+        }
+    }
+
+    function return_news_posts(){
+        $args = array(
+            'numberposts' => 3,
+            'post_status' => 'publish'
+        );
+        $posts = wp_get_recent_posts($args);
+        // wp_get_recent_posts is supposed to return an array
+        // of objects. But sometimes it returns an array of
+        // arrays. We don't need an object and it's easier to
+        // cast down to an array in PHP.
+        return (array) $posts;
+    }
+
+    function process_news_article($post){
+        $char_limit = 194;
+
+        $category = return_news_article_category($post['ID']);
+
+        $link = '/news/'.$category['slug'].'/'.$post['post_name'];
+
+        $title = esc_attr($post['post_title']);
+        $chars_remaining = $char_limit - strlen($title);
+
+        //Remove wordpress pseudo-tags like [caption id=...]
+        $content = preg_replace('/\[.*\]/', '', $post['post_content']);
+        $content = strip_tags($content);
+        $content = break_at_next_word($chars_remaining, $content, $link);
+
+        return array(
+            'category' => $category['name'],
+            'link'     => $link,
+            'title'    => $title,
+            'content'  => $content
+        );
+    }
+
+    function return_news_article_category($id){
+        $categories = get_the_category($id);
+        $default = array(
+            'name' => 'Recent News',
+            'slug' => 'recent-news'
+        );
+
+        if( ! isset($categories[0]) ){ return $default; }
+        if( ! isset($categories[0]->name) ){ return $default; }
+
+        return array(
+            'name' => $categories[0]->name,
+            'slug' => $categories[0]->slug
+        );
+    }
+
+    function return_news_articles_class($i, $per_row){
+        if( ($i - 1) % $per_row == 0 ){
+            $class = 'alpha';
+        }elseif( $i % $per_row  === 0 ){
+            $class = 'omega';
+        }else{
+            $class = '';
+        }
+        return $class;
+    }
+
+    function print_bucket($content, $class = ''){
+        echo '<article class="news-item one-third column '.$class.'">
+                <span class="news-section">'.$content['category'].'</span>
+                <a href="'.$content['link'].'" class="news-title">'.$content['title'].'</a>
+                <span class="news-description">'.$content['content'].'</span>
+                </article>';
+    }
+
+
 //Loop functions
 
     function loop_page_title(){
