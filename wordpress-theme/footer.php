bc77e82f1f060feda0ccf237a2bef4a7f7876c19
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:24:49 -0400

    Making header and footer more generic by moving Main section out and into specific template files

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -6,9 +6,6 @@
  */
 ?>
 
-                </section>
-            </section>
-
         </div>
     </div>
 
--- wordpress-theme/header.php
+++ wordpress-theme/header.php
@@ -83,10 +83,3 @@
                     </div>
                 </div>
             </section>
-
-            <section id="main" class="container clearfix">
-                <section id="sidebar" class="five columns alpha">
-                    <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
-                </section>
-
-                <section id="content-container" class="eleven columns omega">
