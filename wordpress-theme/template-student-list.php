b6e0a537ca335e839883ed9460384c3da54eb22d
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:28:31 -0400

    Moved Main section into student list template

--- wordpress-theme/template-student-list.php
+++ wordpress-theme/template-student-list.php
@@ -9,57 +9,70 @@
 
 ?><?php include(TEMPLATEPATH.'/header.php'); ?>
 
-        <?php the_post(); ?>
+    <section id="main" class="container clearfix">
 
-        <section id="content" <?php post_class('page'); ?>>
+        <section id="sidebar" class="five columns alpha">
 
-            <h1 class="title page_title"><?php the_title(); ?></h1>
-
-            <div class="post_content">
-
-                <?php the_content(); ?>
-
-                <?php
-
-                    $year = get_post_meta($post->ID, 'year', TRUE);	
-                    $data = get_api_data('/api/our-people/students/'.$year);
-
-                    if( empty($data->users) ){
-
-                        echo '<p>There are currently no members to show.</p>';
-
-                    } else {
-                    
-                        foreach( $data->users as $section => $users ){
-
-                            if( $section == 'postdoc' ){
-                                $section = 'Postdoctoral Researcher';
-                            }
-
-                            $section = $section . 's';
-
-                            echo '<h2>'.clean_slug($section).'</h2>';
-
-                            echo '<ul class="student-list '.$year.'">';
-
-                            foreach($users as $user){
-                                if( $user == 'TBD' ){
-                                    continue;
-                                }
-                                echo '<li>'.$user.'</li>';
-                            }
-                            
-                            echo '</ul>';
-
-                        }
-
-                    }
-
-                ?>
-
-            </div>
+            <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
 
         </section>
 
-<?php include(TEMPLATEPATH.'/footer.php'); ?>
+        <section id="content-container" class="eleven columns omega">
 
+            <?php the_post(); ?>
+
+            <section id="content" <?php post_class('page'); ?>>
+
+                <h1 class="title page_title"><?php the_title(); ?></h1>
+
+                <div class="post_content">
+
+                    <?php the_content(); ?>
+
+                    <?php
+
+                        $year = get_post_meta($post->ID, 'year', TRUE);	
+                        $data = get_api_data('/api/our-people/students/'.$year);
+
+                        if( empty($data->users) ){
+
+                            echo '<p>There are currently no members to show.</p>';
+
+                        } else {
+                        
+                            foreach( $data->users as $section => $users ){
+
+                                if( $section == 'postdoc' ){
+                                    $section = 'Postdoctoral Researcher';
+                                }
+
+                                $section = $section . 's';
+
+                                echo '<h2>'.clean_slug($section).'</h2>';
+
+                                echo '<ul class="student-list '.$year.'">';
+
+                                foreach($users as $user){
+                                    if( $user == 'TBD' ){
+                                        continue;
+                                    }
+                                    echo '<li>'.$user.'</li>';
+                                }
+                                
+                                echo '</ul>';
+
+                            }
+
+                        }
+
+                    ?>
+
+                </div>
+
+            </section>
+
+        </section>
+
+    </section>
+
+<?php include(TEMPLATEPATH.'/footer.php'); ?>
