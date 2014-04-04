8a831fac1e6dd501fb44ddec8f8a5d71bb616f59
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:27:39 -0400

    Moved Main section into presentations overview template

--- wordpress-theme/template-presentations-overview.php
+++ wordpress-theme/template-presentations-overview.php
@@ -8,52 +8,66 @@
  */
 ?><?php include(TEMPLATEPATH.'/header.php'); ?>
 
-        <?php the_post(); ?>
+    <section id="main" class="container clearfix">
 
-        <section id="content" <?php post_class('page'); ?>>
+        <section id="sidebar" class="five columns alpha">
 
-            <h1 class="title page_title"><?php the_title(); ?></h1>
-
-            <div class="post_content">
-
-                <?php the_content(); ?>
-
-                <?php
-
-                    $data = get_api_data('/api/our-work/presentations');
-
-                    if( count($data->latest_conference) > 0 ){
-
-                        echo '<h2>Most Recent Conference Presentations</h2>';
-                        echo '<ol class="our-work-list presentations conference-presentations">';
-
-                        foreach($data->latest_conference as $pres){
-                            echo '<li>'.create_conference_presentation($pres).'</li>';
-                        }
-                        
-                        echo '</ol>';
-
-                    }
-
-                    if( count($data->latest_nonconference) > 0 ){
-
-                        $nonconf_array = array();
-
-                        echo '<h2>Most Recent Non-Conference Presentations</h2>';
-                        echo '<ol class="our-work-list presentations nonconference-presentations">';
-
-                        foreach($data->latest_nonconference as $pres){
-                            echo '<li>'.create_nonconference_presentation($pres).'</li>';
-                        }
-
-                        echo '</ol>';
-
-                    }
-
-                ?>
-
-            </div>
+            <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
 
         </section>
+
+        <section id="content-container" class="eleven columns omega">
+
+            <?php the_post(); ?>
+
+            <section id="content" <?php post_class('page'); ?>>
+
+                <h1 class="title page_title"><?php the_title(); ?></h1>
+
+                <div class="post_content">
+
+                    <?php the_content(); ?>
+
+                    <?php
+
+                        $data = get_api_data('/api/our-work/presentations');
+
+                        if( count($data->latest_conference) > 0 ){
+
+                            echo '<h2>Most Recent Conference Presentations</h2>';
+                            echo '<ol class="our-work-list presentations conference-presentations">';
+
+                            foreach($data->latest_conference as $pres){
+                                echo '<li>'.create_conference_presentation($pres).'</li>';
+                            }
+                            
+                            echo '</ol>';
+
+                        }
+
+                        if( count($data->latest_nonconference) > 0 ){
+
+                            $nonconf_array = array();
+
+                            echo '<h2>Most Recent Non-Conference Presentations</h2>';
+                            echo '<ol class="our-work-list presentations nonconference-presentations">';
+
+                            foreach($data->latest_nonconference as $pres){
+                                echo '<li>'.create_nonconference_presentation($pres).'</li>';
+                            }
+
+                            echo '</ol>';
+
+                        }
+
+                    ?>
+
+                </div>
+
+            </section>
+
+        </section>
+
+    </section>
 
 <?php include(TEMPLATEPATH.'/footer.php'); ?>
