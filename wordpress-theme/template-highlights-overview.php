102cfb7cff2b20ca2ca69b7e379cdd828eb13fd4
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:26:31 -0400

    Moved Main section into highlights overview template

--- wordpress-theme/template-highlights-overview.php
+++ wordpress-theme/template-highlights-overview.php
@@ -8,44 +8,58 @@
  */
 ?><?php include(TEMPLATEPATH.'/header.php'); ?>
 
-        <?php the_post(); ?>
+    <section id="main" class="container clearfix">
 
-        <section id="content" <?php post_class('page'); ?>>
+        <section id="sidebar" class="five columns alpha">
 
-            <h1 class="title page_title"><?php the_title(); ?></h1>
-
-            <div class="post_content">
-
-                <?php the_content(); ?>
-
-                <?php
-
-                    $data = get_api_data('/api/our-work/research-and-outreach-highlights');
-
-                    if( isset($data->highlights) && !empty($data->highlights) ){
-
-                        $highlights = array();
-                        foreach($data->highlights as $highlight){
-                            $content = $highlight->title;
-                            if( $highlight->file_path ){
-                                $content .= '<a class="external file-icon file-type-'.$highlight->file_type.'" href="'.$highlight->file_path.'">Download as PDF</a>';
-                            }
-
-                            $highlights[] = '<li>'.$content.'</li>';
-                        }
-
-                        echo '<ol class="our-work-list highlights">'.implode($highlights).'</ol>';
-
-                    }else{
-
-                        echo '<p>There are no highlights to display for the given year.</p>';
-
-                    }
-
-                ?>
-
-            </div>
+            <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
 
         </section>
+
+        <section id="content-container" class="eleven columns omega">
+
+            <?php the_post(); ?>
+
+            <section id="content" <?php post_class('page'); ?>>
+
+                <h1 class="title page_title"><?php the_title(); ?></h1>
+
+                <div class="post_content">
+
+                    <?php the_content(); ?>
+
+                    <?php
+
+                        $data = get_api_data('/api/our-work/research-and-outreach-highlights');
+
+                        if( isset($data->highlights) && !empty($data->highlights) ){
+
+                            $highlights = array();
+                            foreach($data->highlights as $highlight){
+                                $content = $highlight->title;
+                                if( $highlight->file_path ){
+                                    $content .= '<a class="external file-icon file-type-'.$highlight->file_type.'" href="'.$highlight->file_path.'">Download as PDF</a>';
+                                }
+
+                                $highlights[] = '<li>'.$content.'</li>';
+                            }
+
+                            echo '<ol class="our-work-list highlights">'.implode($highlights).'</ol>';
+
+                        }else{
+
+                            echo '<p>There are no highlights to display for the given year.</p>';
+
+                        }
+
+                    ?>
+
+                </div>
+
+            </section>
+
+        </section>
+
+    </section>
 
 <?php include(TEMPLATEPATH.'/footer.php'); ?>
