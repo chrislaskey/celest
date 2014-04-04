21de922cf019258aa8a96d74e905e597d189763c
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:25:36 -0400

    Moved Main section into page template

--- wordpress-theme/page.php
+++ wordpress-theme/page.php
@@ -8,23 +8,37 @@
 
 ?><?php include(TEMPLATEPATH.'/header.php'); ?>
 
-        <section id="content">
+    <section id="main" class="container clearfix">
 
-            <?php the_post(); ?>
+        <section id="sidebar" class="five columns alpha">
 
-            <div <?php post_class('page'); ?>>
-
-                <h1 class="title page_title"><?php the_title(); ?></h1>
-
-                <div class="post_content">
-                    <?php the_content(); ?>
-                    <?php wp_link_pages( array('before' => '<div class="page-link">Pages: ', 'after' => '</div>') ); ?>
-                </div>
-
-            </div>
-
-            <?php comments_template( '', true ); ?>
+            <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
 
         </section>
+
+        <section id="content-container" class="eleven columns omega">
+
+            <section id="content">
+
+                <?php the_post(); ?>
+
+                <div <?php post_class('page'); ?>>
+
+                    <h1 class="title page_title"><?php the_title(); ?></h1>
+
+                    <div class="post_content">
+                        <?php the_content(); ?>
+                        <?php wp_link_pages( array('before' => '<div class="page-link">Pages: ', 'after' => '</div>') ); ?>
+                    </div>
+
+                </div>
+
+                <?php comments_template( '', true ); ?>
+
+            </section>
+
+        </section>
+
+    </section>
 
 <?php include(TEMPLATEPATH.'/footer.php'); ?>
