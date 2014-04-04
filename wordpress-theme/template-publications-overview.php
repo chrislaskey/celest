fdbc98d969ba69cc9dbf65b3f611935e6d4a085b
Chris Laskey <contact@chrislaskey.com>
2014-04-03 20:28:12 -0400

    Moved Main section into publications overview template

--- wordpress-theme/template-publications-overview.php
+++ wordpress-theme/template-publications-overview.php
@@ -8,52 +8,66 @@
  */
 ?><?php include(TEMPLATEPATH.'/header.php'); ?>
 
-        <?php the_post(); ?>
+    <section id="main" class="container clearfix">
 
-        <section id="content" <?php post_class('page'); ?>>
+        <section id="sidebar" class="five columns alpha">
 
-            <h1 class="title page_title"><?php the_title(); ?></h1>
-
-            <div class="post_content">
-
-                <?php the_content(); ?>
-
-                <?php
-
-                    $data = get_api_data('/api/our-work/publications');
-
-                    if( count($data->latest_book) > 0 ){
-
-                        echo '<h2>Most Recent Books and Book Chapters</h2>';
-                        echo '<ol class="our-work-list publications book-publications">';
-
-                        foreach($data->latest_book as $pub){
-                            echo '<li>'.create_book_publication($pub).'</li>';
-                        }
-                        
-                        echo '</ol>';
-
-                    }
-
-                    if( count($data->latest_peer) > 0 ){
-
-                        $nonconf_array = array();
-
-                        echo '<h2>Most Recent Peer-Reviewed Publications</h2>';
-                        echo '<ol class="our-work-list publications peer-publications">';
-
-                        foreach($data->latest_peer as $pub){
-                            echo '<li>'.create_peer_publication($pub).'</li>';
-                        }
-
-                        echo '</ol>';
-
-                    }
-
-                ?>
-
-            </div>
+            <?php include(TEMPLATEPATH.'/sidebar.php'); ?>
 
         </section>
+
+        <section id="content-container" class="eleven columns omega">
+
+            <?php the_post(); ?>
+
+            <section id="content" <?php post_class('page'); ?>>
+
+                <h1 class="title page_title"><?php the_title(); ?></h1>
+
+                <div class="post_content">
+
+                    <?php the_content(); ?>
+
+                    <?php
+
+                        $data = get_api_data('/api/our-work/publications');
+
+                        if( count($data->latest_book) > 0 ){
+
+                            echo '<h2>Most Recent Books and Book Chapters</h2>';
+                            echo '<ol class="our-work-list publications book-publications">';
+
+                            foreach($data->latest_book as $pub){
+                                echo '<li>'.create_book_publication($pub).'</li>';
+                            }
+                            
+                            echo '</ol>';
+
+                        }
+
+                        if( count($data->latest_peer) > 0 ){
+
+                            $nonconf_array = array();
+
+                            echo '<h2>Most Recent Peer-Reviewed Publications</h2>';
+                            echo '<ol class="our-work-list publications peer-publications">';
+
+                            foreach($data->latest_peer as $pub){
+                                echo '<li>'.create_peer_publication($pub).'</li>';
+                            }
+
+                            echo '</ol>';
+
+                        }
+
+                    ?>
+
+                </div>
+
+            </section>
+
+        </section>
+
+    </section>
 
 <?php include(TEMPLATEPATH.'/footer.php'); ?>
